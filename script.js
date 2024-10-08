5012c019705e3c61d4acc7e613831818708f7afb
DWL22 <candra.icy@gmail.com>
2024-10-08 15:50:03 +0800

    tsp bali pertama

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    menuIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+});
